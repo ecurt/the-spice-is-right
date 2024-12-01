d5f25057577300b356c5696a4897dcf4daa32b07
Evan Curtis <evanlucc@gmail.com>
2024-12-01 10:39:54 -0700

    Changed env variable name for cloud hosting

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -1,1 +1,13 @@
-# Here
+#!/bin/bash
+
+# DO NOT PUSH THIS FILE TO GITHUB
+# This file contains sensitive information and should be kept private
+
+# TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
+PG_URI="postgresql://users_db_uwh0_user:9qko3Jso7RDg5xS9hBeqtZLEioM99sFD@dpg-ct69arilqhvc73agkfag-a.oregon-postgres.render.com/users_db_ng0m"
+
+# Execute each .sql file in the directory
+for file in src/init_data/*.sql; do
+    echo "Executing $file..."
+    psql $PG_URI -f "$file"
+done
--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -27,7 +27,7 @@ const hbs = handlebars.create({
 // database configuration
 
 const dbConfig = {
-  host: process.env.DATABASE_URL, // the database server
+  host: process.env.POSTGRES_HOST, // the database server
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
